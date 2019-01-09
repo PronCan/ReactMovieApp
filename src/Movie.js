1bb8eaf3f678e6085bd4d5c65fd4c00b17b3e04b
PronCan <twojini98@gmail.com>
2019-01-10 02:21:54 +0900

    2
    Movie.js  -  MoviePoster추가

--- src/Movie.js
+++ src/Movie.js
@@ -4,8 +4,19 @@ import './Movie.css';
 class Movie extends Component {
     render() {
         return (
-            <h1>hello this is a movie</h1>
+            <div>
+                <MoviePoster />
+                <h1>hello this is a movie</h1>
+            </div>
         )
+    }
+}
+
+class MoviePoster extends Component {
+    render() {
+        return(
+            <img src="https://t1.daumcdn.net/cfile/tistory/9974FE3A5AFE995F0A" />
+        );
     }
 }
 
